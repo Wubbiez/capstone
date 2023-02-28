a3c4f9759f8597492538d19193c360afc189076c
Wubbiez <108950491+Wubbiez@users.noreply.github.com>
2023-02-27 19:27:37 -0500

    Revert "Cory branch"

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
-
-import { BrowserRouter } from 'react-router-dom';
 // our app below
 ReactDOM.render(
-  <BrowserRouter>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
