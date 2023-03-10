26d7f53089401b4a4ee82cc15111248ec993414d
Wubbiez <108950491+Wubbiez@users.noreply.github.com>
2023-03-09 22:03:26 -0500

    Revert "Cory branch"

--- src/Components/Success.js
+++ src/Components/Success.js
@@ -1,29 +1,11 @@
-import React, {useEffect, useState} from "react";
-import { Drawer, Box, Grid, Typography, Stack, Card, CardContent, CardActions, CardMedia, Button, ButtonGroup, Link } from '@mui/material';
-
-import {getOrderProductsByOrderId} from '../api/apirequests.js';
+import React, {useEffect} from "react";
+import { Link } from "react-router-dom";
 
 const Success = ({order, setOrder}) => {
-    const [orderId, setOrderId] = useState();
-    const [orderProducts, setOrderProducts] = useState([]);
-
     useEffect(() => {
         const url = new URL(window.location.href);
         const sessionId = url.searchParams.get("session_id");
-        const order_id = localStorage.getItem('order_id');
 
-      if (order_id) {
-          setOrderId(order_id);
-          console.log("order_id is", order_id);
-      }
-
-        getOrderProductsByOrderId(order_id).then((orderProducts) => {
-            console.log("orderProducts are", orderProducts);
-            setOrderProducts(orderProducts);
-            console.log("orderProducts are still", orderProducts);
-          });
-
-          
         const fetchData = async () => {
             try {
                 const response = await fetch(`http://localhost:3001/success?session_id=${sessionId}`, {
@@ -39,59 +21,16 @@ const Success = ({order, setOrder}) => {
         };
 
         fetchData();
-        // localStorage.removeItem('order_id');
-        // setOrder(null);
+        localStorage.removeItem('order_id');
+        setOrder(null);
 
     }, []);
 
 
     return (
         <div>
-            <Typography variant ="h1"
-            sx={{
-                marginTop: '10vh',
-            }}>Success!  Thank you for buying the items below</Typography>
-            <Grid container spacing={2} direction="column" style={{ overflowY: 'scroll'}}>
-
-          
-{orderProducts.map((orderProduct) => {
-
-  return (
-  
-    <Card key={orderProduct.id}sx={{
-        
-        display: 'flex',
-        flexFlow: 'row',
-        }}>
-
-        <CardContent sx={{
-          backgroundColor: '#F5F5F5',
-          color: '#333333',
-          width: '250px',
-          padding: '16px',
-          display: 'flex',
-          flexFlow: 'column',
-          borderBottom: "1px solid #666666"
-          }}>
-
-            <Typography variant="h5"
-            >Product Id:  {orderProduct.productId}</Typography>
-            <Typography variant="h6">Price ${orderProduct.price}</Typography>
-            <Typography variant="h6">Quantity {orderProduct.quantity}</Typography>
-
-        </CardContent>
-
-    </Card>
-  )})};
-
-         
-         <Link to='/products'>
-             <Button variant="contained">Products
-             </Button>
-         </Link>
-        
-
-</Grid>
+            <h1>Thank you for your purchase!</h1>
+            <Link to='/products'>Return to Products</Link>
         </div>
     )
 }
