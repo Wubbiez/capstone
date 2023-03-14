0857e39024d01c1bd0a76c4b1c22f538fe71b0f5
Santiago Acuna <acuna2394@gmail.com>
2023-03-13 23:21:54 -0500

    updated product tables with new products and categories

--- src/Components/SampleProducts.js
+++ src/Components/SampleProducts.js
@@ -1,8 +1,8 @@
-import React, {useEffect, useState} from "react";
-import {Link} from "react-router-dom";
-import {Card, Paper, Grid, styled, Typography, Button, CardContent, Box} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Card, Paper, Grid, styled, Typography, Button, CardContent, Box } from "@mui/material";
 import AddToOrderButton from "./Buttons/AddToOrderButton.js";
-import {getAllProducts} from "../api/apirequests.js";
+import { getAllProducts } from "../api/apirequests.js";
 // import EditOrderProductButton from "./Buttons/EditOrderProductButton.js";
 import UpdateQuantityButton from "./Buttons/UpdateQuantityButton.js";
 import EditProductButton from "./Buttons/EditProductButton.js";
@@ -11,7 +11,7 @@ import DeleteOrderProductButton from "./Buttons/DeleteOrderProductButton.js";
 import CheckoutButton from "./Buttons/CheckoutButton.js";
 import SingleProductModal from "./SingleProductModal.js";
 import EmptyCartButton from "./Buttons/EmptyCartButton.js";
-function SampleProducts({order, setOrder, user, setIsAdmin, isAdmin}) {
+function SampleProducts({ order, setOrder, user, setIsAdmin, isAdmin }) {
     const [products, setProducts] = useState([]);
     const [refresh, setRefresh] = useState(false);
 
@@ -24,8 +24,8 @@ function SampleProducts({order, setOrder, user, setIsAdmin, isAdmin}) {
                 setOrder(order_id);
             }
         }
-            const checkAdmin = localStorage.getItem('user-is_admin');
-        if(checkAdmin==="true"){
+        const checkAdmin = localStorage.getItem('user-is_admin');
+        if (checkAdmin === "true") {
             setIsAdmin(true);
         }
         getAllProducts().then((products) => {
@@ -41,7 +41,7 @@ function SampleProducts({order, setOrder, user, setIsAdmin, isAdmin}) {
         <React.Fragment>
 
             <Grid container spacing={2} sx={{
-                                      backgroundColor: '#f5f5f5'
+                backgroundColor: '#f5f5f5'
             }}>
                 {products.sort((a, b) => a.product_id - b.product_id).map((product) => {
 
@@ -55,66 +55,66 @@ function SampleProducts({order, setOrder, user, setIsAdmin, isAdmin}) {
                                     alignItems: 'center',
                                 }}>
                                     <img src={product.image} alt={product.description}
-                                         style={{maxWidth: '100%', height: 'auto'}}/>
+                                        style={{ maxWidth: '100%', height: 'auto' }} />
                                     <Typography variant="h1">{product.title}</Typography>
                                     <Typography variant="h4">$ {product.price}</Typography>
                                     <Box sx={{
-                                    display: 'flex',
-                                    flexFlow: 'column',
-                                    maxWidth: '50%',
-                                    
-                                }}>
-                                        <SingleProductModal variant="contained" 
-                                                            userId={1}
-                                                            order_id={order}
-                                                            title={product.title}
-                                                            description={product.description}
-                                                            price={product.price}
-                                                            image={product.image}
-                                                            product_id={product.product_id}
-                                                            category={product.category}
-                                                            in_stock={product.in_stock}
-                                                            status="created"
-                                                            quantity={1}
-                                                            setOrder={setOrder}
-                                                            stripe_id={product.stripe_id}
-                                                            refresh={refresh}
-                                                            setRefresh={setRefresh}
-                                            />
+                                        display: 'flex',
+                                        flexFlow: 'column',
+                                        maxWidth: '50%',
+
+                                    }}>
+                                        <SingleProductModal variant="contained"
+                                            userId={1}
+                                            order_id={order}
+                                            title={product.title}
+                                            description={product.description}
+                                            price={product.price}
+                                            image={product.image}
+                                            product_id={product.product_id}
+                                            category={product.category}
+                                            in_stock={product.in_stock}
+                                            status="created"
+                                            quantity={1}
+                                            setOrder={setOrder}
+                                            stripe_id={product.stripe_id}
+                                            refresh={refresh}
+                                            setRefresh={setRefresh}
+                                        />
                                         <AddToOrderButton userId={1} product_id={product.product_id} status="created"
-                                                        price={product.price} quantity={1} setOrder={setOrder}
-                                                        stripe_id={product.stripe_id} order_id={order} setRefresh={setRefresh}
+                                            price={product.price} quantity={1} setOrder={setOrder}
+                                            stripe_id={product.stripe_id} order_id={order} setRefresh={setRefresh}
 
                                         />
 
-                            
-                                            
+
+
                                         <Box display="flex" alignItems="center" justifyContent="center"
-                                            style={{margin: '8px 0'}}>
+                                            style={{ margin: '8px 0' }}>
                                             <UpdateQuantityButton order_id={order} orderProductId={product.product_id}
-                                                                price={product.price} setRefresh={setRefresh} refresh={refresh}/>
+                                                price={product.price} setRefresh={setRefresh} refresh={refresh} />
                                         </Box>
-                                        
+
                                         <Box display="flex" alignItems="center" justifyContent="center"
-                                            style={{margin: '8px 0'}}>
-                                            { isAdmin &&   <EditProductButton variant="contained" color="secondary" title={product.title}
-                                                           description={product.description}
-                                                           price={product.price} image={product.image}
-                                                           product_id={product.product_id} category={product.category}
-                                                           in_stock={product.in_stock} stripeId={product.stripe_id} /> }
+                                            style={{ margin: '8px 0' }}>
+                                            {isAdmin && <EditProductButton variant="contained" color="secondary" title={product.title}
+                                                description={product.description}
+                                                price={product.price} image={product.image}
+                                                product_id={product.product_id} category={product.category}
+                                                in_stock={product.in_stock} stripeId={product.stripe_id} />}
                                         </Box>
-                                        
+
                                         <Box display="flex" alignItems="center" justifyContent="center"
-                                            style={{margin: '8px 0'}}>
+                                            style={{ margin: '8px 0' }}>
                                             {/*<DeleteProductButton product_id={product.product_id} setRefresh={setRefresh}/>*/}
                                         </Box>
-                                         <Box display="flex" alignItems="center" justifyContent="center"
-                                         style={{margin: '8px 0'}}>
-                                        <DeleteOrderProductButton product_id={product.product_id} order_id={order} setRefresh={setRefresh} />
-                                    </Box>
-                                     {order && <Box display="flex" alignItems="center" justifyContent="center" style={{margin: '8px 0'}}>
-                                        <EmptyCartButton order_id={order} setRefresh={setRefresh} />
-                                    </Box>}
+                                        <Box display="flex" alignItems="center" justifyContent="center"
+                                            style={{ margin: '8px 0' }}>
+                                            <DeleteOrderProductButton product_id={product.product_id} order_id={order} setRefresh={setRefresh} />
+                                        </Box>
+                                        {order && <Box display="flex" alignItems="center" justifyContent="center" style={{ margin: '8px 0' }}>
+                                            <EmptyCartButton order_id={order} setRefresh={setRefresh} />
+                                        </Box>}
                                     </Box>
 
                                 </CardContent>
--- src/api/endpoints/products.js
+++ src/api/endpoints/products.js
@@ -1,69 +1,189 @@
-import express from "express";
+import client from "../client.js";
+import stripe0 from "stripe";
+import { config } from "dotenv";
+config();
+async function createProduct({ title, description, price, image, inStock, category }) {
+    try {
+        const { rows: [product] } = await client.query(`
+            INSERT INTO products(title, description, price, "image", "in_stock", category)
+            VALUES($1, $2, $3, $4, $5, $6)
+            RETURNING *;
+        `, [title, description, price, image, inStock, category]);
+        return product;
+    } catch (error) {
+        throw error;
+    }
+}
+async function getAllProducts() {
+    try {
+        const { rows: products } = await client.query(`
+            SELECT *
+            FROM products;
+        `);
+        return products;
+    } catch (error) {
+        throw error;
+    }
+}
 
-import {
-    checkIfProductInStock,
-    destroyProduct,
+async function getProductById(productId) {
+    try {
+        const { rows: [product] } = await client.query(`
+            SELECT *
+            FROM products
+            WHERE product_id = $1;
+        `, [productId]);
+        if (!product) {
+            return null;
+        }
+        return product;
+    } catch (error) {
+        throw error;
+    }
+}
+
+async function updateProduct({ product_id: id, title, description, price, image, inStock, category, stripe_id }) {
+    try {
+        const { rows: [product] } = await client.query(`
+            UPDATE products
+            SET title = $1,
+                description = $2,
+                price = $3,
+                "image" = $4,
+                "in_stock" = $5,
+                category = $6,
+                stripe_id = $7
+            WHERE product_id = $8
+            RETURNING *;
+        `, [title, description, price, image, inStock, category, stripe_id, id]);
+        return product;
+    } catch (error) {
+        throw error;
+    }
+}
+
+async function updateStripe({ product_id: id, title, description, price, image, inStock, category, stripe_id }) {
+    const stripe = stripe0(process.env.STRIPE_API_KEY);
+    const productObject = {
+        name: title,
+        description,
+        images: [image],
+        active: inStock,
+        metadata: {
+            category: category
+        },
+    }
+
+    try {
+        const response = await stripe.products.update(id.toString(), productObject);
+        console.log(response);
+        console.log(price)
+        //
+        // update the stripe price object with the new price
+        const priceResponse = await stripe.prices.create(
+            {
+                product: response.id,
+                unit_amount: Math.round(price * 100),
+                currency: "usd",
+            }
+        );
+        console.log(priceResponse);
+        return priceResponse;
+        //
+
+
+    } catch (error) {
+        console.log(error);
+    }
+
+
+    try {
+        const { rows: [product] } = await client.query(`
+            UPDATE products
+            SET title       = $1,
+                description = $2,
+                price       = $3,
+                "image"     = $4,
+                "in_stock"  = $5,
+                category    = $6,
+                stripe_id   = $7
+            WHERE product_id = $8
+            RETURNING *;
+        `, [title, description, price, image, inStock, category, stripe_id, id]);
+        return product;
+    } catch (error) {
+        throw error;
+    }
+}
+
+
+async function destroyProduct(productId) {
+    try {
+        const { rows: [product] } = await client.query(`
+            DELETE FROM products
+            WHERE product_id = $1
+            RETURNING *;
+        `, [productId]);
+        return product;
+    } catch (error) {
+        throw error;
+    }
+}
+
+async function getProductsByCategory(category) {
+    try {
+        const { rows: products } = await client.query(`
+            SELECT *
+            FROM products
+            WHERE category = $1;
+        `, [category]);
+        return products;
+    } catch (error) {
+        throw error;
+    }
+}
+
+async function checkIfProductInStock(productId) {
+    try {
+        const { rows: [product] } = await client.query(`
+            SELECT "in_stock"
+            FROM products
+            WHERE product_id = $1;
+        `, [productId]);
+        return product;
+    } catch (error) {
+        throw error;
+    }
+}
+
+async function updateOrderProduct({ product_id: id, title, description, price, image, inStock, category, stripe_id }) {
+    try {
+        const { rows: [product] } = await client.query(`
+            UPDATE products
+            SET title = $1,
+                description = $2,
+                price = $3,
+                "image" = $4,
+                "in_stock" = $5,
+                category = $6,
+                stripe_id = $7
+            WHERE product_id = $8
+            RETURNING *;
+        `, [title, description, price, image, inStock, category, stripe_id, id]);
+        return product;
+    } catch (error) {
+        throw error;
+    }
+}
+
+
+export {
+    createProduct,
     getAllProducts,
     getProductById,
     updateProduct,
+    destroyProduct,
+    getProductsByCategory,
+    checkIfProductInStock,
     updateStripe
-} from "../../server/db/components/products.js";
-
-
-const productsRouter = express.Router();
-
-productsRouter.get("/", async (req, res, next) => {
-    try {
-        const products = await getAllProducts();
-
-        res.send(products);
-    } catch (error) {
-        next(error);
-    }
-})
-
-productsRouter.get("/:id", async (req, res, next) => {
-    try {
-        const {id} = req.params;
-        const product = await getProductById(id);
-        res.send(product);
-    } catch (error) {
-        next(error);
-    }
-});
-productsRouter.patch("/:id", async (req, res, next) => {
-
-        try {
-            const {id} = req.params;
-            const {product_id, title,description,price,image,inStock,category, stripe_id} = req.body
-            const updateStriped = await updateStripe({product_id: id, title, description, price, image, inStock, category, stripe_id});
-            const product = await updateProduct({product_id: id, title, description, price, image, inStock, category, stripe_id: updateStriped.id})
-
-            res.send(updateStriped);
-        } catch (error) {
-            next(error);
-        }
-
-});
-
-productsRouter.get("/:product_id/stock", async (req, res, next) => {
-    try {
-        const {product_id} = req.params;
-        console.log(product_id);
-        const product = await checkIfProductInStock(product_id);
-        res.send(product);
-    } catch (error) {
-        next(error);
-    }
-})
-
-productsRouter.delete("/:id", async (req, res, next) => {
-    try {
-        const {id} = req.params;
-        const product = await destroyProduct(id);
-        res.send(product);
-    } catch (error) {
-        next(error);
-    }
-});
-export default productsRouter;
+}
