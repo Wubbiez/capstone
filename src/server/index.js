012f5c71ee208d7383765f0bd68362d345be5d09
Zachary Collie <zachc4@vt.edu>
2023-03-21 13:59:25 -0400

    DB changes

--- src/server/index.js
+++ src/server/index.js
@@ -2,42 +2,47 @@ import apiRouter from "../api/index.js";
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
-import {updateOrder} from "./db/components/orders.js";
+import { updateOrder } from "./db/components/orders.js";
 import stripe0 from "stripe";
-import {config} from "dotenv";
+import { config } from "dotenv";
+import { createProxyMiddleware } from "http-proxy-middleware";
 
 config();
 
-
 const app = express();
-
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-
-}   );
 
 // Middleware
 app.use(morgan("dev"));
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
+app.use(
+    "/api/**",
+    createProxyMiddleware({
+        target: "https://34.227.96.218:3001",
+        changeOrigin: true,
+        onProxyRes: (proxyRes, req, res) => {
+            proxyRes.headers["Access-Control-Allow-Origin"] = "*";
+        },
+    })
+);
 
-app.post('/success', async (req, res) => {
-    const {session_id} = req.query;
+app.post("/success", async (req, res) => {
+    const { session_id } = req.query;
     try {
         const stripe = stripe0(process.env.STRIPE_API_KEY);
         const session = await stripe.checkout.sessions.retrieve(session_id);
         const order_id = session.client_reference_id;
         console.log(order_id);
-        if (session.payment_status === 'paid') {
-            await updateOrder({orderId: order_id, status: 'paid'});
+        if (session.payment_status === "paid") {
+            await updateOrder({ orderId: order_id, status: "paid" });
             // res.redirect(`/orders/${order_id}`);
         } else {
-            console.log('Payment not successful');
-            res.redirect('/products');
+            console.log("Payment not successful");
+            res.redirect("/products");
         }
     } catch (error) {
-        console.error('Error in stripe checkout', error);
+        console.error("Error in stripe checkout", error);
         res.status(500).end();
     }
 });
@@ -56,7 +61,7 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
     console.error(err);
     res.status(err.status || 500).send(err.message || "Internal server error.");
-})
+});
 
 // Export
 export default app;
--- src/server/server.js
+++ src/server/server.js
@@ -18,21 +18,6 @@ const options = {
     key: fs.readFileSync(SSL_KEY_PATH),
 }
 
-app.use('/api/**', createProxyMiddleware({ target: 'https://34.227.96.218:3001', changeOrigin: true, onProxyRes: (proxyRes, req, res) => {
-proxyRes.headers['Access-Control-Allow-Origin'] = '*';
-    }
-}));
-
-app.use(cors(
-    {
-        origin: "*",
-        methods: "*",
-        allowedHeaders: "*",
-        credentials: true,
-    }
-));
-
-
 
 const server = https.createServer(options, app);
 
