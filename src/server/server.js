9aaed16bb817113ebb6146353742eb4d1528b6c8
Zachary Collie <zachc4@vt.edu>
2023-03-21 11:09:30 -0400

    DB changes

--- src/server/server.js
+++ src/server/server.js
@@ -1,16 +1,25 @@
 import app from "./index.js";
-import http from "http";
+import https from "https";
 import chalk from "chalk";
 import fs from "fs";
 import {config} from "dotenv";
+import {createProxyMiddleware} from "http-proxy-middleware";
 
 config();
 
 const PORT = process.env["PORT"] ?? 3001;
 
-// const SSL_CERT_PATH = "./cert.pem";
-// const SSL_KEY_PATH = "./key.pem";
-const server = http.createServer(app);
+const SSL_CERT_PATH = "./cert.pem";
+const SSL_KEY_PATH = "./key.pem";
+
+const options = {
+    cert: fs.readFileSync(SSL_CERT_PATH),
+    key: fs.readFileSync(SSL_KEY_PATH),
+}
+
+app.use('/api', createProxyMiddleware({ target: 'http://34.227.96.218:3001', changeOrigin: true }));
+
+const server = https.createServer(options, app);
 
 server.listen(PORT, () => {
     console.log(
