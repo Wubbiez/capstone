d3c3cc8dfcd0d6030681074144643514f2bb55fe
Zachary Collie <zachc4@vt.edu>
2023-03-21 15:55:24 -0400

    DB changes

--- src/server/server.js
+++ src/server/server.js
@@ -21,7 +21,7 @@ const options = {
 
 
 app.use('/api/**', createProxyMiddleware({
-    target: 'https://34.227.96.218:3001/api',
+    target: 'https://34.227.96.218:3001/',
     changeOrigin: false,
     onProxyRes: (proxyRes, req, res) => {
         proxyRes.headers['Access-Control-Allow-Origin'] = '*';
