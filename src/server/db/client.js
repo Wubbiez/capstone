51affffd78f38ab9a352f697e7c1a0b7581e206c
Zachary Collie <zachc4@vt.edu>
2023-03-20 23:08:45 -0400

    DB changes

--- src/server/db/client.js
+++ src/server/db/client.js
@@ -1,12 +1,19 @@
 import pkg from 'pg';
 
 const {Pool} = pkg;
-
-const connectionString = process.env.DATABASE_URL || 'https://localhost:5432/capstone';
-
 const client = new Pool({
-    connectionString,
-    ssl: process.env.NODE_ENV === 'production' ? {rejectUnauthorized: false} : undefined,
+    user: 'wubbie',
+    password: 'Zaqwsx101!?!',
+    host: 'gadgetgalaxy.ccl2wv2rdpbg.us-east-1.rds.amazonaws.com',
+    database: 'gadgetgalaxy',
+    port: 5432,
+    ssl: true, // set to true if you are using SSL
 });
+// const connectionString = process.env.DATABASE_URL || `postgresql://wubbie:Zaqwsx101!?!@gadgetgalaxy.ccl2wv2rdpbg.us-east-1.rds.amazonaws.com:5432/gadgetgalaxy`;
+//
+// const client = new Pool({
+//     connectionString,
+//     ssl: process.env.NODE_ENV === 'production' ? {rejectUnauthorized: false} : undefined,
+// });
 
 export default client;
