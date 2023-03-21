bc419aa4c710ceb6530f90b5ae93301cb4d4c837
Zachary Collie <zachc4@vt.edu>
2023-03-21 03:24:28 -0400

    DB changes

--- src/server/server.js
+++ src/server/server.js
@@ -10,12 +10,7 @@ const PORT = process.env["PORT"] ?? 3001;
 
 const SSL_CERT_PATH = "./cert.pem";
 const SSL_KEY_PATH = "./key.pem";
-const server = https.createServer(
-    {
-        key: fs.readFileSync(SSL_KEY_PATH),
-        cert: fs.readFileSync(SSL_CERT_PATH),
-    },
-    app);
+const server = https.createServer(app);
 
 server.listen(PORT, () => {
     console.log(
