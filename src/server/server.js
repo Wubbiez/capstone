9a590c07e91e33629e4e99ca224e1aa8297c1d01
Zachary Collie <zachc4@vt.edu>
2023-03-21 03:14:07 -0400

    DB changes

--- src/server/server.js
+++ src/server/server.js
@@ -1,12 +1,21 @@
 import app from "./index.js";
-import https from "http";
+import https from "https";
 import chalk from "chalk";
+import fs from "fs";
 import {config} from "dotenv";
 
 config();
 
 const PORT = process.env["PORT"] ?? 3001;
-const server = https.createServer(app);
+
+const SSL_CERT_PATH = "../../cert.pem";
+const SSL_KEY_PATH = "../../key.pem";
+const server = https.createServer(
+    {
+        key: fs.readFileSync(SSL_KEY_PATH),
+        cert: fs.readFileSync(SSL_CERT_PATH),
+    },
+    app);
 
 server.listen(PORT, () => {
     console.log(
