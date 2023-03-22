05cd98271c6d1e0c7a2ea8a481cbd63d6e528afa
Zachary Collie <zachc4@vt.edu>
2023-03-22 03:10:44 -0400

    DB changes

--- src/server/server.js
+++ src/server/server.js
@@ -19,13 +19,13 @@ const PORT = process.env["PORT"] ?? 3001;
 //     key: fs.readFileSync(SSL_KEY_PATH),
 // }
 
-// app.use('/api/**', createProxyMiddleware({
-//     target: 'https://34.227.96.218:3001',
-//     changeOrigin: false,
-//     headers: {
-//         'Referrer-Policy': 'no-referrer',
-//     },
-// }));
+app.use('/api/**', createProxyMiddleware({
+    target: 'https://34.227.96.218:3001',
+    changeOrigin: false,
+    headers: {
+        'Referrer-Policy': 'no-referrer',
+    },
+}));
 
 
 
