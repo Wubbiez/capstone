f3e045c93e7a67606651b83f204b3d48ea147f07
Zachary Collie <zachc4@vt.edu>
2023-03-22 00:11:49 -0400

    DB changes

--- src/server/server.js
+++ src/server/server.js
@@ -22,7 +22,7 @@ const options = {
 
 
 app.use('/api/**', createProxyMiddleware({
-    target: 'https://localhost:3001/',
+    target: 'https://localhost:3001',
     changeOrigin: false,
     headers: {
         'Referrer-Policy': 'no-referrer',
