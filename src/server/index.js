a2926abc508d4d7fe167870a1ca4c1084efbc3c8
Zachary Collie <zachc4@vt.edu>
2023-03-23 01:41:11 -0400

    DB changes

--- src/server/index.js
+++ src/server/index.js
@@ -62,7 +62,7 @@ app.use((err, req, res, next) => {
 
 // Set the Access-Control-Allow-Origin header to allow requests from your domain name
 app.use(function(req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "gadgetgalaxy.link");
+    res.setHeader("Access-Control-Allow-Origin", "api.gadgetgalaxy.link");
     next();
 });
 
