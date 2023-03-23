6208dec9371112ecca240c2066a1c367d5e1e881
Zachary Collie <zachc4@vt.edu>
2023-03-23 00:51:25 -0400

    DB changes

--- src/api/endpoints/isAdmin.js
+++ src/api/endpoints/isAdmin.js
@@ -2,7 +2,6 @@ import jwt from "jsonwebtoken";
 
 const {JWT_SECRET} = process.env;
 export const isAdmin = (req, res, next) => {
-    console.log(req.headers.authorization);
     const token = req.headers.authorization.split(' ')[1];
     const decodedToken = jwt.verify(token, JWT_SECRET);
     const isAdmin = decodedToken.is_admin;
